42d54e9cfcc3bdc10580a159893913dc96a44ac2
Wilson Fung <wilson_fung1996@yahoo.com>
2023-06-16 13:54:08 -0400

    create Event model

--- happeningconnoisseur/main_app/models.py
+++ happeningconnoisseur/main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+  name = models.CharField(max_length=100)
+  location = models.CharField(max_length=100)
+  date = models.DateField('Event Date')
+  type = models.CharField(max_length=100)
