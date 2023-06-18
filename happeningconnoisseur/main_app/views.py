a0bfe59e91a7c33b068bcb5e9a893f89f92ab3f2
Jeremy Cox <jeremy@theoutlet.org>
2023-06-18 11:19:47 -0400

    add login, logout, and sign up to nav bar

--- happeningconnoisseur/main_app/views.py
+++ happeningconnoisseur/main_app/views.py
@@ -2,23 +2,29 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from .models import Event
+from django.contrib.auth.decorators import login_required
 
 # Define the home view
 
 
 def home(request):
     # Include an .html file extension - unlike when rendering EJS templates
-    return render(request, 'home.html')
+    return render(request, 'events/index.html')
 
+
+@login_required
 def events_index(request):
     events = Event.objects.all()
     return render(request, 'events/index.html', {
         'events': events
     })
 
+
+@login_required
 def events_detail(request, event_id):
     event = Event.objects.get(id=event_id)
-    return render(request, 'events/detail.html', { 'event': event })
+    return render(request, 'events/detail.html', {'event': event})
+
 
 def signup(request):
     error_message = ''
@@ -38,6 +44,7 @@ def signup(request):
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
+
 
 def about(request):
     return render(request, 'about.html')
