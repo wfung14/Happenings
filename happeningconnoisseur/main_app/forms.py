ffdd85ae41b0fac6abe2a7d39adcefb1356977c9
Jeremy Cox <jeremy@theoutlet.org>
2023-06-18 12:47:27 -0400

    create add event functionality

---
+++ happeningconnoisseur/main_app/forms.py
@@ -0,0 +1,10 @@
+
+from django.forms import ModelForm
+from .models import Event
+
+
+class EventForm(ModelForm):
+    class Meta:
+        model = Event
+        fields = ['name', 'location',
+                  'date', 'type']
--- happeningconnoisseur/main_app/models.py
+++ happeningconnoisseur/main_app/models.py
@@ -2,24 +2,27 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Event(models.Model):
-  name = models.CharField(max_length=100)
-  location = models.CharField(max_length=100)
-  date = models.DateField('Event Date')
-  type = models.CharField(max_length=100)
 
-  def __str__(self):
-    return f'{self.name}'
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    date = models.DateField('Event Date')
+    type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name}'
+
 
 class Vendor(models.Model):
-  name = models.CharField(max_length=100)
-  location = models.CharField(max_length=100)
-  email = models.CharField(max_length=100)
-  # food = models.CharField(max_length=250)
-  # drinks = models.CharField(max_length=250)
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    # food = models.CharField(max_length=250)
+    # drinks = models.CharField(max_length=250)
 
-  def __str__(self):
-    return self.name
-  
-  def get_absolute_url(self):
-    return reverse('vendors_detail', kwargs={'pk': self.id})
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('vendors_detail', kwargs={'pk': self.id})
