8320dce4b2771fc5d4bebc0ff69980065a7b1205
Jeremy Cox <jeremy@theoutlet.org>
2023-06-22 10:29:10 -0400

    make photo field required

--- happeningconnoisseur/main_app/models.py
+++ happeningconnoisseur/main_app/models.py
@@ -31,10 +31,3 @@ class Event(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'event_id': self.id})
-
-# class Photo(models.Model):
-#     url = models.CharField(max_length=200)
-#     event = models.ForeignKey(Event, on_delete=models.CASCADE)
-
-#     def __str__(self):
-#         return f"Photo for event_id: {self.event_id} @{self.url}"
